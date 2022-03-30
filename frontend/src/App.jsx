3db548af49a03cfed37236aef064c790e90d6b0f
bastienwcs <bastien.krettly@wildcodeschool.com>
2022-03-30 11:37:33 +0200

    fullstack example

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,28 @@
-import Home from "@pages/Home";
-
+import axios from "axios";
 import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [datas, setDatas] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`${import.meta.env.VITE_BACKEND_URL}/items`)
+      .then((response) => {
+        setDatas(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <Home />
-      <p>coucou</p>
+      <ul>
+        {datas.map((data) => (
+          <li>{JSON.stringify(data)}</li>
+        ))}
+      </ul>
     </div>
   );
 }
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,8 @@
-const path = require("path");
+/* eslint-disable import/no-extraneous-dependencies */
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@assets": path.resolve(__dirname, "src/assets"),
-      "@components": path.resolve(__dirname, "src/components"),
-      "@pages": path.resolve(__dirname, "src/pages"),
-      "@services": path.resolve(__dirname, "src/services"),
-    },
-  },
 });
